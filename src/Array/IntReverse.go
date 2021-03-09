0ec4cf8968a7af733376ea4902a39ac3a8874ca8
cqmike <1535985458@qq.com>
2021-03-10 00:00:50 +0800

    add int reverse

--- src/Array/IntReverse.go
+++ src/Array/IntReverse.go
@@ -1,28 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func reverse(x int) int {
-	if x == 0 {
-		return 0
-	}
-
-	isAbs := true
-	if x < 0 {
-		isAbs = false
-	}
-
 	temp := 0
-	for i := 1; x > 10; i++ {
-		temp = temp + (i-1)*10 + (x % (i * 10))
+	for x != 0 {
+		// 每次乘10且加上取余数
+		temp = temp*10 + x%10
 		x = x / 10
+		// 判断是否溢出
+		if temp < math.MinInt32 || temp > math.MaxInt32 {
+			return 0
+		}
 	}
-
-	if isAbs {
-		return temp
-	} else {
-		return -temp
-	}
+	return temp
 }
 
 func main() {
